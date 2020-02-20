f6cc74e6dce84da589068c516f6d8c723bd69a6a
“dziekan30” <dziekan30@gmail.com>
2020-02-20 16:30:59 -0600

    new

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,36 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>{{ message }}</h1>
+    <div v-for="contact in contacts">
+      <h2>{{ contacts.first_name }}</h2>
+    </div>
+    <p>{{ contacts }}</p>
   </div>
+ 
+
 </template>
 
+<style>
+</style>
+
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+let axios = require("axios");
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+  data: function() {
+    return {
+      message: "Welcome to Vue.js!",
+      contacts: []
+    };
+  },
+  created: function() {
+    axios.get("/api/contacts")
+    .then(response => {
+    this.contacts = response.data;
+    });
+  },
+  methods: {
+
   }
-}
+};
 </script>
